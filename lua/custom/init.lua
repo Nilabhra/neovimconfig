14a2da10045e89442b2b0bfc35a619ca37d8e276
Nilabhra Roy Chowdhury <nilabhraroy@gmail.com>
2023-10-08 23:30:17 +0400

    feat: enabled both absolute and relative line numbers.

---
+++ lua/custom/init.lua
@@ -0,0 +1,6 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- vim.opt.signcolumn = "number"
+-- vim.opt.numberwidth = 3
+-- vim.opt.statuscolumn = "%=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum < 10 ? v:lnum . '  ' : v:lnum) : ''}%=%s"
+vim.opt.statuscolumn = "%l %r"
