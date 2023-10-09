941d82bcaf2eaecb688304a68f56b5161fce0074
Nilabhra Roy Chowdhury <nilabhraroy@gmail.com>
2023-10-09 12:38:42 +0400

    feat: added code folding column based on indent.

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,5 +1,6 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.foldmethod = "indent"
 -- vim.opt.signcolumn = "number"
 -- vim.opt.numberwidth = 3
-vim.opt.statuscolumn = "%l %r"
+vim.opt.statuscolumn = "%l %r %C"
